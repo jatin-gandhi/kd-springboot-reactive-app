e404ca6c2c752cf435e750bc8069c45d8192f2f6
Jatin Gandhi <jatin.gandhi@kickdrumtech.com>
2024-12-16 19:51:39 +0530

    bolierplate for sprinboot reactive app

---
+++ src/main/java/com/example/kd/springboot/KdSpringbootApplication.java
@@ -0,0 +1,14 @@
+package com.example.kd.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KdSpringbootApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(KdSpringbootApplication.class, args);
+        System.out.println("Running Template Code .....");
+    }
+}
---
+++ src/test/java/com/example/kd/springboot/KdSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kd.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KdSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
